532d501de1f5a32e351ef2108bd0b630f296fc3f
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2021-05-11 09:40:48 +0100

    2.

---
+++ ZoeConroy_s000196433/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoeConroy_s000196433
+{
+    class Game
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double MetacriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public double Price { get; set; }
+        public string Game_Image { get; set; }
+
+    }
+}
